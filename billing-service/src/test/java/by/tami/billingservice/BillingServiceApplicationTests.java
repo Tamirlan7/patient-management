f8483caf7b22e1a0dffec45a71d7ac54123c2bad
Tamirlan7 <zukasaevtamirlan1@gmail.com>
2026-03-14 07:18:14 +0500

    init

---
+++ billing-service/src/test/java/by/tami/billingservice/BillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tami.billingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
